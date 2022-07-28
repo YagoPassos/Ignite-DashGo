39bfdda28896ed716ac5464aaf25f8dbd77fcd73
Yago Passos <yp.anjos@hotmail.com>
2022-07-27 23:49:25 -0300

    React Query Added

--- dashgo/.next/static/chunks/webpack.js
+++ dashgo/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a58cf736c4512580"; }
+/******/ 		__webpack_require__.h = function() { return "5d658e00dd9131f9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- dashgo/src/pages/_app.tsx
+++ dashgo/src/pages/_app.tsx
@@ -2,20 +2,26 @@ import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
 
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
 import { makeServer } from '../services/mirage'
 
-if (process.env.NODE_ENV === 'development'){
+if (process.env.NODE_ENV === 'development') {
     makeServer()
 }
 
+const queryClient = new QueryClient
+
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <SidebarDrawerProvider>
+        <QueryClientProvider client={queryClient}>
             <ChakraProvider theme={theme}>
-                <Component {...pageProps} />
+                <SidebarDrawerProvider>
+                    <Component {...pageProps} />
+                </SidebarDrawerProvider >
             </ChakraProvider>
-        </SidebarDrawerProvider>
+        </QueryClientProvider>
+
     )
 }
 
