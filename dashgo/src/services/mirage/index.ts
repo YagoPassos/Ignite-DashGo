e5140c8364aab248c862d98a313fea9166d683d8
Yago Passos <yp.anjos@hotmail.com>
2022-08-02 14:36:11 -0300

    Mirag Index Updated

--- dashgo/src/services/mirage/index.ts
+++ dashgo/src/services/mirage/index.ts
@@ -39,21 +39,23 @@ export function makeServer() {
             this.namespace = 'api'
             this.timing = 750;
 
-            this.get('/users', function(schema, request) {
+            this.get('/users', function (schema, request) {
                 const { page = 1, per_page = 10 } = request.queryParams
 
                 const total = schema.all('user').length
-        
+
                 const pageStart = (Number(page) - 1) * Number(per_page)
                 const pageEnd = pageStart + Number(per_page)
-        
+
                 const users = this.serialize(schema.all('user'))
-                  .users.slice(pageStart, pageEnd)
-        
+                    .users
+                    .sort((a, b) => { a.created_at - b.created_at })
+                    .slice(pageStart, pageEnd)
+
                 return new Response(
-                  200,
-                  { 'x-total-count': String(total) },
-                  { users }
+                    200,
+                    { 'x-total-count': String(total) },
+                    { users }
                 )
             })
             this.get('/users/:id');
